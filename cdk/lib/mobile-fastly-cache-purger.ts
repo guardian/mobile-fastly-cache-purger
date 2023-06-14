34795ca026c1527d59d2980030907d70eff2f123
silvacb <silvija.blaslov@gmail.com>
2023-06-14 16:25:40 +0100

    Add policy

--- cdk/lib/mobile-fastly-cache-purger.ts
+++ cdk/lib/mobile-fastly-cache-purger.ts
@@ -4,10 +4,25 @@ import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import type { App } from 'aws-cdk-lib';
 import {Duration} from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam'
 
 export class MobileFastlyCachePurger extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
+
+		const executionRole = new iam.Role(this, 'ExecutionRole', {
+			assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+			path: "/",
+			inlinePolicies: {
+				Conf: new iam.PolicyDocument({
+					statements: [
+						new iam.PolicyStatement({
+							actions: [ 'ssm:GetParametersByPath' ],
+							resources: [ `arn:aws:ssm:${scope.region}:${scope.account}:parameter/cache-purger/${this.stage}` ]
+						})
+					] }),
+			}
+		})
 
 		new GuLambdaFunction(this, 'mobile-fastly-cache-purger', {
 			handler: 'PurgerLambda::handleRequest',
@@ -21,6 +36,7 @@ export class MobileFastlyCachePurger extends GuStack {
 			runtime: Runtime.JAVA_11,
 			app: 'mobile-fastly-cache-purger',
 			fileName: `mobile-fastly-cache-purger.jar`,
+			role: executionRole,
 		});
 	}
 }
