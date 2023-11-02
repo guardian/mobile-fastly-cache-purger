30ca7d590531a0bfc7851a132b3b8e69bb3586ba
silvacb <silvija.blaslov@gmail.com>
2023-11-02 09:48:49 +0000

    Remove facia specific config, add facia role to config

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -2,7 +2,7 @@ import com.gu.conf.{ConfigurationLoader, SSMConfigurationLocation}
 import com.gu.{AppIdentity, AwsIdentity}
 import com.typesafe.config.{Config => Conf}
 
-case class Config(fastlyServiceId: String, fastlyApiKey: String)
+case class Config(fastlyServiceId: String, fastlyApiKey: String, faciaRole: String)
 
 object Config {
 
@@ -17,22 +17,22 @@ object Config {
     println("Loading facia-purger config...")
 
     val config = fetchConfiguration()
-    Config(config.getString("FastlyServiceId"), config.getString("FastlyAPIKey"))
+    Config(config.getString("FastlyServiceId"), config.getString("FastlyAPIKey"), config.getString("apis.facia.role"))
   }
 }
 
-case class FaciaConfig(role: String, environment: String, s3Bucket: String)
-
-object FaciaConfig {
-  private def fetchConfiguration(): Conf = {
-    val identity = AppIdentity.whoAmI(defaultAppName = "mobile-fastly-cache-purger")
-    ConfigurationLoader.load(identity) {
-      case AwsIdentity(_, _, stage, _) => SSMConfigurationLocation(s"/cache-purger/$stage/")
-    }
-  }
-    def load(): FaciaConfig = {
-
-      val config = fetchConfiguration()
-      FaciaConfig(config.getString("apis.facia.role"), config.getString("CODE"), config.getString("facia-tool-store"))
-      }
-}
+//case class FaciaConfig(role: String, environment: String, s3Bucket: String)
+//
+//object FaciaConfig {
+//  private def fetchConfiguration(): Conf = {
+//    val identity = AppIdentity.whoAmI(defaultAppName = "mobile-fastly-cache-purger")
+//    ConfigurationLoader.load(identity) {
+//      case AwsIdentity(_, _, stage, _) => SSMConfigurationLocation(s"/cache-purger/$stage/")
+//    }
+//  }
+//    def load(): FaciaConfig = {
+//
+//      val config = fetchConfiguration()
+//      FaciaConfig(config.getString("apis.facia.role"), config.getString("CODE"), config.getString("facia-tool-store"))
+//      }
+//}
