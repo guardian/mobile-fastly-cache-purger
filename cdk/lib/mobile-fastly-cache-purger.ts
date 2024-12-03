eb3c18f2d718474519e82c91c003ed9409e46e3d
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2024-12-03 15:04:08 +0000

    Add typescript definition for docker lambda

--- cdk/lib/mobile-fastly-cache-purger.ts
+++ cdk/lib/mobile-fastly-cache-purger.ts
@@ -1,23 +1,41 @@
-import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import type {GuStackProps } from '@guardian/cdk/lib/constructs/core';
-import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
-import { GuardianAwsAccounts } from '@guardian/private-infrastructure-config';
-import type { App } from 'aws-cdk-lib';
+import type {GuStackProps} from '@guardian/cdk/lib/constructs/core';
+import {GuStack} from '@guardian/cdk/lib/constructs/core';
+import {GuLambdaFunction} from '@guardian/cdk/lib/constructs/lambda';
+import {GuardianAwsAccounts} from '@guardian/private-infrastructure-config';
+import type {App} from 'aws-cdk-lib';
 import {Duration} from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
-import { Topic } from 'aws-cdk-lib/aws-sns';
+import {Repository, TagMutability} from 'aws-cdk-lib/aws-ecr'
+import iam from 'aws-cdk-lib/aws-iam';
+import type { FunctionProps} from 'aws-cdk-lib/aws-lambda';
+import {Code, Function, Handler, Runtime} from 'aws-cdk-lib/aws-lambda';
+import lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
+import {Topic} from 'aws-cdk-lib/aws-sns';
 import {SqsSubscription} from "aws-cdk-lib/aws-sns-subscriptions";
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import sqs from 'aws-cdk-lib/aws-sqs';
 
+interface GuFunctionDockerProps  extends Omit<FunctionProps, "code" | "handler" | "runtime">{
+	app: string;
+	repositoryArn: string;
+}
+class GuLambdaDockerFunction extends Function {
+	constructor(scope: GuStack, id: string, props: GuFunctionDockerProps) {
+		const repository = Repository.fromRepositoryArn(scope, id, props.repositoryArn)
+		const code = Code.fromEcrImage(repository)
+		super(scope, id, {
+			...props,
+			code: code,
+			runtime: Runtime.FROM_IMAGE,
+			handler: Handler.FROM_IMAGE
+		})
+	}
+
+}
 export class MobileFastlyCachePurger extends GuStack {
 
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
 		const faciaID = this.stage == "CODE" ? "StorageConsumerRole-1JWVQ2NTELFT7" : "StorageConsumerRole-1R9GQEVJIM323";
-
 
 		const executionRole: iam.Role = new iam.Role(this, 'ExecutionRole', {
 			assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
@@ -52,6 +70,48 @@ export class MobileFastlyCachePurger extends GuStack {
 			}
 		})
 
+		const ecrRepository = new Repository(this, 'mobile-fastly-cache-purger', {
+			repositoryName: 'mobile-fastly-cache-purger',
+			imageScanOnPush: true,
+			imageTagMutability: TagMutability.IMMUTABLE
+		})
+
+		new iam.Role(this, 'CIRole', {
+			assumedBy:  new iam.FederatedPrincipal(
+				`arn:aws:iam::${GuardianAwsAccounts.Mobile}:oidc-provider/token.actions.githubusercontent.com`,
+				{
+					"StringEquals": { "token.actions.githubusercontent.com:aud": "sts.amazonaws.com" },
+					"StringLike": { "token.actions.githubusercontent.com:sub": "repo:guardian/mobile-fastly-cache-purger:*"}
+				},
+				"sts:AssumeRoleWithWebIdentity"
+				),
+			inlinePolicies: {
+				ecrToken: new iam.PolicyDocument({
+					statements: [
+						new iam.PolicyStatement({
+							actions: ['ecr:GetAuthorizationToken'],
+							resources: ['*']
+						})
+					]
+				}),
+				ecrUpload: new iam.PolicyDocument({
+					statements: [
+						new iam.PolicyStatement({
+							actions: [
+								'ecr:CompleteLayerUpload',
+								'ecr:UploadLayerPart',
+								'ecr:InitiateLayerUpload',
+								'ecr:BatchCheckLayerAvailability',
+								'ecr:PutImage'
+							],
+							resources: [ecrRepository.repositoryArn]
+						})
+					]
+				})
+
+			}
+		})
+
 		const handler: GuLambdaFunction = new GuLambdaFunction(this, 'mobile-fastly-cache-purger', {
 			handler: 'PurgerLambda::handleRequest',
 			functionName: `mobile-fastly-cache-purger-cdk-${this.stage}`,
@@ -64,6 +124,19 @@ export class MobileFastlyCachePurger extends GuStack {
 			runtime: Runtime.JAVA_11,
 			app: 'mobile-fastly-cache-purger',
 			fileName: `mobile-fastly-cache-purger.jar`,
+			role: executionRole,
+		});
+
+		const lambdaFunction: GuLambdaDockerFunction = new GuLambdaDockerFunction(this, 'mobile-fastly-cache-purger', {
+			functionName: `mobile-fastly-cache-purger-cdk-${this.stage}`,
+			timeout: Duration.seconds(60),
+			environment: {
+				App: 'mobile-fastly-cache-purger',
+				Stack: this.stack,
+				Stage: this.stage,
+			},
+			app: 'mobile-fastly-cache-purger',
+			repositoryArn: 'arn',
 			role: executionRole,
 		});
 
@@ -90,6 +163,6 @@ export class MobileFastlyCachePurger extends GuStack {
 		const eventSource: lambdaEventSources.SqsEventSource = new lambdaEventSources.SqsEventSource(queue);
 
 		handler.addEventSource(eventSource);
-
+		lambdaFunction.addEventSource(eventSource);
 	}
 }
