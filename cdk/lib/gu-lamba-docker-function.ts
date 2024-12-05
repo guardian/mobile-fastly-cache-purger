f4f9428f86ae0aa404e16afaa9557da15da7af66
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2024-12-05 10:59:10 +0000

    Inherit the props

--- cdk/lib/gu-lamba-docker-function.ts
+++ cdk/lib/gu-lamba-docker-function.ts
@@ -1,26 +1,29 @@
 import type {GuStack} from "@guardian/cdk/lib/constructs/core";
 import {Repository} from "aws-cdk-lib/aws-ecr";
 import type { FunctionProps} from "aws-cdk-lib/aws-lambda";
-import {Code, DockerImageCode, DockerImageFunction, Function, Handler, Runtime} from "aws-cdk-lib/aws-lambda";
+import {Code, Function, Handler, Runtime} from "aws-cdk-lib/aws-lambda";
 
 interface GuFunctionDockerProps  extends Omit<FunctionProps, "code" | "handler" | "runtime">{
-    app: string;
     repositoryArn: string;
     repositoryName: string;
     imageTag: string;
 }
-export class GuLambdaDockerFunction extends DockerImageFunction{
+export class GuLambdaDockerFunction extends Function{
     constructor(scope: GuStack, id: string, props: GuFunctionDockerProps) {
          super(scope, id, {
-            code: DockerImageCode.fromEcr(
-                Repository.fromRepositoryAttributes(scope, 'mobile-fastly-cache-purger-ecr', {
-                    repositoryArn: props.repositoryArn,
-                    repositoryName: props.repositoryName,
-                }), {
-                    tagOrDigest:  props.imageTag,
-                }
-            ),
-        });
+             code: Code.fromEcrImage(
+                 Repository.fromRepositoryAttributes(scope, 'mobile-fastly-cache-purger-ecr', {
+                     repositoryArn: props.repositoryArn,
+                     repositoryName: props.repositoryName,
+                 }), {
+                     tagOrDigest: props.imageTag,
+                 }
+             ),
+             runtime: Runtime.FROM_IMAGE,
+             handler: Handler.FROM_IMAGE,
+             ...props,
+         }
+        );
     }
 }
 
